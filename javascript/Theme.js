2551dd3293816ad93583d02f9f542f6e235feefa
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2018-10-16 20:20:43 -0500

    organisation

--- javascript/Theme.js
+++ javascript/Theme.js
@@ -9,7 +9,15 @@ link.href = "css/win98.css";
 link.type = "text/css";
 link.rel = "stylesheet";
 link.media = "screen,print";
-
+var totalCount = 44;
+function ChangeIt()
+{
+var num = Math.ceil( Math.random() * totalCount );
+document.body.background = 'newbg/'+num+'.jpg';
+document.body.style.background = "URL('newbg/ ("+ num +").jpg')";
+console.log("doing bg: " + num);
+//document.body.style.backgroundSize = "auto";// Background repeat
+}
 document.getElementsByTagName( "head" )[0].appendChild( link );
 var cssLink = document.createElement("link");
 cssLink.href = "style.css"; 
