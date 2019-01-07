0df13bae95d608187de7dfe5c60d07d33eedbc70
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-01-07 15:23:55 -0600

    mah bad

--- javascript/Splash.js
+++ javascript/Splash.js
@@ -1,8 +1,8 @@
 	function randomSplash(){
 
 		var textArray = [
-    	'IE Users are banned.',
-    	'Abort, Retry, Fail?',
+    		'IE Users are banned.',
+    		'Abort, Retry, Fail?',
 		'No websites were harmed in the making of this website.',
 		'Fun Fact: There was only a 1/10 chance of seeing this.',
 		'Designed for 4:3 Monitors.',
@@ -12,11 +12,11 @@
 		'We do not acknowledge Fortnite\'s existence.',
 		'Now on github.',
 		'Half-life > Half-life 2',
-		'it would apear that you exist, congrats',
+		'it would appear that you exist, congrats',
 		'Version 6',
 		'Mac OS > Windows 9x',
 		'PowerPC > Wintel',
-		'1280x1024 is god\'s resoltion'
+		'1280x1024 is god\'s resolution'
 		];
 		var randomNumber = Math.floor(Math.random()*textArray.length);
 		document.getElementById("splash").textContent = textArray[randomNumber];
