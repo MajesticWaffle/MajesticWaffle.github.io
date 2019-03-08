71835ff0c0e093c6ed0ef9d85d2dd52652d1003c
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-03-08 17:35:22 -0600

    clean up + more hmm links

--- google.php
+++
@@ -1,6 +0,0 @@
-<?php
-$content = file_get_contents('https://drive.google.com/embeddedfolderview?id=1QAAgBX7lhURj3RN0Tc47hNBvXJuadVNH#list');
-$content = str_replace('</title>','</title><base href="https://www.google.com/calendar/" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="macos.css" /></head>', $content);
-echo $content;
-?>
--- javascript/dirrectory.js
+++ javascript/dirrectory.js
@@ -1,19 +1,33 @@
 function games(){
 	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1mb8XTzqCPMTbrhPRnfLS-kARaqMclhJB#list";
-	alert(window.frames["yeets"].location);
+	document.getElementById('click').play();
 }
 function oses(){
-	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1M9c3jLdIVnjxzYCbbSgm0Wz9tZ2FS-9E#list";	
+	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1M9c3jLdIVnjxzYCbbSgm0Wz9tZ2FS-9E#list";
+	document.getElementById('click').play();	
 }
 function tools(){
 	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1ONpd5o1Q1iSoq4rkDjE63PH82C2dk6u1#list";	
+	document.getElementById('click').play();
 }
 function midi(){
 	var textArray = [
 			'https://megamindchan.org/legacy/',
-			'http://friendlydads.net/'
-			
+			'http://friendlydads.net/',
+			'http://www.staggeringbeauty.com/',
+			'http://111111111111111111111111111111111111111111111111111111111111.com/',
+			'https://hooooooooo.com/',
+			'http://beesbeesbees.com/',
+			'http://timecube.2enp.com/',
+			'https://www.templeos.org/',
+			'https://pointerpointer.com/',
+			'https://crouton.net/',
+			'https://html5zombo.com/',
+			'http://tunnelsnakes.com/',
+			'http://tunnelsnakes.com/',
+			'https://bonzi.link/'
 		];
 	var randomNumber = Math.floor(Math.random()*textArray.length);
 	window.frames["yeets"].location = textArray[randomNumber];
+	document.getElementById('click').play();
 }
