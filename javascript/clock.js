a89d5d522b5ad4e006b1ccfe25c388fb58eeb26b
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-03-17 12:54:25 -0500

    fixed Clock

--- javascript/clock.js
+++ javascript/clock.js
@@ -3,10 +3,14 @@ function startTime() {
   var h = today.getHours();
   var m = today.getMinutes();
   m = checkTime(m);
-  if(h > 12)
-	  document.getElementById('clock').innerHTML = (h - 12) + ":" + m + " PM ";
-  else
+  if(h < 12 && h > 0) //1 - 11 displays AM
 	  document.getElementById('clock').innerHTML = h + ":" + m + " AM ";
+  if(h > 12) // 13 - 23 display PM
+	  document.getElementById('clock').innerHTML = (h - 12) + ":" + m + " PM ";
+  if(h == 12) //special case for noon
+	  document.getElementById('clock').innerHTML = h + ":" + m + " PM ";
+  if(h == 0) //special case for midnight
+	  document.getElementById('clock').innerHTML = 12 + ":" + m + " AM ";
   var t = setTimeout(startTime, 500);
 }
 
