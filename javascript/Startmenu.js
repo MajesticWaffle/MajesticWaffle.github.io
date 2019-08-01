3202a2c68b22c9b86a7ca142c3f6c867b87ffe75
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-01 17:39:21 -0500

    fixed Startmenu.js (Theme Select)

--- javascript/Startmenu.js
+++ javascript/Startmenu.js
@@ -1,8 +1,8 @@
 var themetoggle = false;
 function test(){
 	if(themetoggle)
-		$('head').append('<link rel="stylesheet" type="text/css" href="css/win98.css">');
+		$('head').append('<link rel="stylesheet" type="text/css" href="/css/win98.css">');
 	else
-		$('head').append('<link rel="stylesheet" type="text/css" href="css/winxp.css">');
+		$('head').append('<link rel="stylesheet" type="text/css" href="/css/winxp.css">');
 	themetoggle = !themetoggle;
 }
