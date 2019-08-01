d6248214427df6784729f3c8e74dc57ed50db4b3
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-01 17:52:31 -0500

    added macos theme to theme select!

--- javascript/Startmenu.js
+++ javascript/Startmenu.js
@@ -1,8 +1,13 @@
-var themetoggle = false;
+var themeIndex = 1;
+var themeMax = 2;
+var themeArray = [
+'<link rel="stylesheet" type="text/css" href="/css/win98.css">',
+'<link rel="stylesheet" type="text/css" href="/css/winxp.css">',
+'<link rel="stylesheet" type="text/css" href="/css/macos.css">'
+]
 function test(){
-	if(themetoggle)
-		$('head').append('<link rel="stylesheet" type="text/css" href="/css/win98.css">');
-	else
-		$('head').append('<link rel="stylesheet" type="text/css" href="/css/winxp.css">');
-	themetoggle = !themetoggle;
+	$('head').append(themeArray[themeIndex]);
+	themeIndex++;
+	if(themeIndex > themeMax)
+		themeIndex = 0;
 }
