52e41e027351e6fc73082417b3ee26c5750e5de4
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-02 17:02:23 -0500

    more mobile changes
    added link to new mobile on mobile error
    
    made 404 mobile version

---
+++ javascript/mobiledetect404.js
@@ -0,0 +1,5 @@
+//detect mobile
+if (/Mobile|Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    // Take the user to a different screen here.
+	window.location.replace("../mobile404.html");
+}
