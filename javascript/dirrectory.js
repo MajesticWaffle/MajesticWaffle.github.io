245e5563c5dcaa30322a811d41d4a8ae6894e218
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-01 19:17:52 -0500

    mobile changes

--- javascript/dirrectory.js
+++ javascript/dirrectory.js
@@ -26,3 +26,17 @@ function midi(){
 	window.frames["yeets"].location = textArray[randomNumber];
 	document.getElementById('click').play();
 }
+
+
+function mgames(){
+	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1mb8XTzqCPMTbrhPRnfLS-kARaqMclhJB#grid";
+	document.getElementById('click').play();
+}
+function moses(){
+	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1M9c3jLdIVnjxzYCbbSgm0Wz9tZ2FS-9E#grid";
+	document.getElementById('click').play();	
+}
+function mtools(){
+	window.frames["yeets"].location = "https://drive.google.com/embeddedfolderview?id=1ONpd5o1Q1iSoq4rkDjE63PH82C2dk6u1#grid";	
+	document.getElementById('click').play();
+}
