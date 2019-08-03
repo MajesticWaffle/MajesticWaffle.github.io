10d6288c91414db55ffb930c61ae156e6d04739f
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-03 18:05:53 -0500

    Added cookies to theme selection

--- javascript/Startmenu.js
+++ javascript/Startmenu.js
@@ -1,13 +1,18 @@
 var themeIndex = 1;
 var themeMax = 2;
 var themeArray = [
-'<link rel="stylesheet" type="text/css" href="/css/win98.css">',
-'<link rel="stylesheet" type="text/css" href="/css/winxp.css">',
-'<link rel="stylesheet" type="text/css" href="/css/macos.css">'
+'<link rel="stylesheet" type="text/css" href="css/win98.css">',
+'<link rel="stylesheet" type="text/css" href="css/winxp.css">',
+'<link rel="stylesheet" type="text/css" href="css/macos.css">'
 ]
-function test(){
+function loadCookieTheme(){
+	themeIndex = parseInt(document.cookie);
+	$('head').append(themeArray[themeIndex]);
+}
+function changeTheme(){
 	$('head').append(themeArray[themeIndex]);
 	themeIndex++;
 	if(themeIndex > themeMax)
 		themeIndex = 0;
+	document.cookie = themeIndex.toString();
 }
