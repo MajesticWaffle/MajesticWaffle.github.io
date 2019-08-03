4486a2ee728a9fbade8ef3c3da764b5bef1acedd
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-03 18:13:48 -0500

    a

--- javascript/Startmenu.js
+++ javascript/Startmenu.js
@@ -1,19 +1,15 @@
-var themeIndex = 1;
+var themeIndex = 0;
 var themeMax = 2;
 var themeArray = [
 '<link rel="stylesheet" type="text/css" href="/css/win98.css">',
 '<link rel="stylesheet" type="text/css" href="/css/winxp.css">',
 '<link rel="stylesheet" type="text/css" href="/css/macos.css">'
 ]
-function loadCookieTheme(){
-	console.log(document.cookie);
-	$('head').append(themeArray[themeIndex]);
-}
+
 function changeTheme(){
 	console.log("setting theme: " + themeIndex);
 	$('head').append(themeArray[themeIndex]);
 	themeIndex++;
 	if(themeIndex > themeMax)
 		themeIndex = 0;
-	document.cookie = themeIndex.toString();
 }
