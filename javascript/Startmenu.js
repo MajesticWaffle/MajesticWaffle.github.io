66001723fc76a5d04318c04fc107514163544e15
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-08-03 18:07:01 -0500

    Update Startmenu.js

--- javascript/Startmenu.js
+++ javascript/Startmenu.js
@@ -1,9 +1,9 @@
 var themeIndex = 1;
 var themeMax = 2;
 var themeArray = [
-'<link rel="stylesheet" type="text/css" href="css/win98.css">',
-'<link rel="stylesheet" type="text/css" href="css/winxp.css">',
-'<link rel="stylesheet" type="text/css" href="css/macos.css">'
+'<link rel="stylesheet" type="text/css" href="/css/win98.css">',
+'<link rel="stylesheet" type="text/css" href="/css/winxp.css">',
+'<link rel="stylesheet" type="text/css" href="/css/macos.css">'
 ]
 function loadCookieTheme(){
 	themeIndex = parseInt(document.cookie);
