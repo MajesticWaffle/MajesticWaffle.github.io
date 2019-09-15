d41d00ed88c65505874fdd963a3fa5a984925281
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-09-15 15:14:14 -0500

    Update TestBSOD.js

--- javascript/TestBSOD.js
+++ javascript/TestBSOD.js
@@ -2,8 +2,8 @@ var clickCount = 0;
 function requestError(){
 	clickCount++;
 	if(clickCount == 10){
-		//throw "User Requested Crash";
-		test();
+		clickCount = 0;
+		throw "User Requested Crash";
 	}
 	else{
 	document.getElementById("splash").textContent = "Click " + (10-clickCount) + " more times to force a BSOD";
