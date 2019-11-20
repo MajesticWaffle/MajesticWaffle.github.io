71fdd65e13fd8042c97c779ac9dbd0049e5ac587
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2019-11-20 13:23:20 -0600

    fixed the thing

--- javascript/TestBSOD.js
+++ javascript/TestBSOD.js
@@ -1,7 +1,11 @@
 var easterAppend = '<link rel="stylesheet" type="text/css" href="/css/easteregg.css">'
 var bsodAppend = '<link rel="stylesheet" type="text/css" href="/css/bsod.css">'
+var thing = 1;
 function requestError(){
+	if(thing >= 10){
 	$('head').append(easterAppend);
 	$('head').append(bsodAppend);
+	}
+	thing++;
 }
 	
