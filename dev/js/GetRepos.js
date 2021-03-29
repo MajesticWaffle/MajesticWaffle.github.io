5439cddc85785b272a838799caa140b0b97139f2
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2021-03-29 08:09:43 -0500

    Update GetRepos.js

--- dev/js/GetRepos.js
+++ dev/js/GetRepos.js
@@ -50,10 +50,51 @@ function BuildRepoList(){
 			listDiv.appendChild(textLabel);
 		})
 	})
-	
+
+	$.getJSON('https://api.github.com/users/ThiccIndustries/repos',{},function(data){
+		
+		data.forEach(v => {
+			if(String(v.description).includes("!tignore"))
+				return;
+
+			if(v.name == removeSelf)
+				return;
+			
+			if(v.name.length > largest)
+				largest = v.name.length
+		})
+		
+		data.forEach(v => {
+			if(String(v.description).includes("!tignore"))
+			return;
+
+			if(v.name == removeSelf || v.description.includes("!tignore"))
+				return;
+			
+			var newLabel = v.name.replace(/-/g, " ");
+			
+			for(var i = 0; i < (largest - v.name.length) + 1; i++){
+				newLabel += " ";
+			}
+			
+			
+			textLabel = document.createElement("p");
+			textLabel.textContent = newLabel;
+			
+			pageLabel = document.createElement("a");
+			pageLabel.textContent = "[Github Page]";
+			pageLabel.href = v.html_url;
+			pageLabel.target = "_blank";
+			
+			textLabel.appendChild(pageLabel);
+			
+			listDiv.appendChild(textLabel);
+		})
+	})
+
 	githubLink = document.createElement("a");
 	githubLink.href = "https://github.com/MajesticWaffle/";
 	githubLink.textContent = "Github Profile";
 	githubLink.target = "_blank";
 	document.getElementById("Profile").appendChild(githubLink);
-}	
+}
